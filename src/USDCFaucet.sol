093313840ce96cc498030b786f16f7728157222f
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2024-01-17 02:53:17 -0500

    add USDC Faucet
    Described in documentation: https://plume.gitbook.io/plume/getting-started/token-faucets

---
+++ src/USDCFaucet.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+interface IERC20 {
+    function transfer(address to, uint256 value) external returns (bool);
+}
+
+contract USDCFaucet {
+    uint256 public tokenAmount = 100000 * 10 ** 6;
+    IERC20 public tokenInstance;
+    event TokensWithdrawn(address indexed recipient, uint256 amount);
+
+    constructor(address _tokenInstance) {
+        tokenInstance = IERC20(_tokenInstance);
+    }
+
+    function requestTokens() public {
+        tokenInstance.transfer(msg.sender, tokenAmount);
+        emit TokensWithdrawn(msg.sender, tokenAmount);
+    }
+}
