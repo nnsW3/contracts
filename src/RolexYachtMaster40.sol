49e04d0871f220c08eb0e526523ef4186511f4f9
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2024-01-24 19:00:49 -0500

    upgrade Solidity compiler version from 0.8.19 to 0.8.23

--- src/RolexYachtMaster40.sol
+++ src/RolexYachtMaster40.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.23;
 
 import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import {ERC721} from "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
@@ -8,13 +8,13 @@ contract RolexYachtMaster40 is Ownable, ERC721 {
     error NFTAlreadyMinted();
     bool private _minted;
 
-    constructor() ERC721("Rolex Yacht-Master 40", "") {}
+    constructor() Ownable(msg.sender) ERC721("Rolex Yacht-Master 40", "") {}
 
-    function mint(address owner) public onlyOwner returns (uint256) {
+    function mint() public onlyOwner returns (uint256) {
         if (_minted) {
             revert NFTAlreadyMinted();
         }
-        _safeMint(owner, 0);
+        _safeMint(owner(), 0);
         _minted = true;
         return 0;
     }
