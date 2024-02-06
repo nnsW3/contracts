f582acf534334498d8cf7e4bf5e533b21bd1c617
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2024-02-06 18:35:17 -0500

    add remappings for @openzeppelin-contracts

--- src/RolexYachtMaster40.sol
+++ src/RolexYachtMaster40.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
-import {ERC721} from "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin-contracts/access/Ownable.sol";
+import {ERC721} from "@openzeppelin-contracts/token/ERC721/ERC721.sol";
 
 contract RolexYachtMaster40 is Ownable, ERC721 {
     error NFTAlreadyMinted();
