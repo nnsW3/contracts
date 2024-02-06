906299608eb2a386dc4adfbbb86bc0e4a3438921
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2024-02-05 22:58:32 -0500

    add example commemorative token contract for asset issuers

---
+++ src/CommemorativeToken.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {ERC721} from "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+
+contract CommemorativeToken is ERC721 {
+    uint256 private _nextTokenId;
+
+    constructor() ERC721("Commemorative Token", "CNFT") {}
+
+    function mint() public returns (uint256) {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+        return tokenId;
+    }
+}
