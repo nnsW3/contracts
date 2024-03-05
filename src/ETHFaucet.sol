c6653c3336b0190aedb2c7fd9734f74739d30a1f
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2024-03-04 23:17:33 -0500

    add ETH Faucet

---
+++ src/ETHFaucet.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {Ownable} from "@openzeppelin-contracts/access/Ownable.sol";
+
+contract ETHFaucet is Ownable {
+    uint256 public constant tokenAmount = 0.001 ether;
+    event ETHWithdrawn(address indexed recipient, uint256 amount);
+
+    constructor() Ownable(msg.sender) {}
+
+    function sendETH(address payable _recipient) external onlyOwner {
+        require(address(this).balance >= tokenAmount, "Not enough tokens in faucet");
+        _recipient.transfer(tokenAmount);
+        emit ETHWithdrawn(_recipient, tokenAmount);
+    }
+
+    receive() external payable {}
+}
