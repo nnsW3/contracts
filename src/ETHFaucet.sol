5374d38cf1e9ae7009c91024ce6a3e8fe6f6f341
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2024-03-07 19:03:22 -0500

    increase faucet amount to 0.01 ETH

--- src/ETHFaucet.sol
+++ src/ETHFaucet.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.23;
 import {Ownable} from "@openzeppelin-contracts/access/Ownable.sol";
 
 contract ETHFaucet is Ownable {
-    uint256 public constant tokenAmount = 0.001 ether;
+    uint256 public constant tokenAmount = 0.01 ether;
     event ETHWithdrawn(address indexed recipient, uint256 amount);
 
     constructor() Ownable(msg.sender) {}
