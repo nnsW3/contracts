71e4b12262bade51e0337c95d5191e88d0c0dd34
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2024-03-16 17:43:01 -0400

    add testnet USDC token
    Fake ERC-20 based USDC stablecoin token with decimals = 6.

---
+++ src/USDC.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {ERC20} from "@openzeppelin-contracts/token/ERC20/ERC20.sol";
+
+contract USDC is ERC20 {
+    constructor(uint256 initialSupply) ERC20("USDC", "USDC") {
+        _mint(msg.sender, initialSupply * (10 ** 6));
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+}
