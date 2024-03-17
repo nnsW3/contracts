d68a96ca6089771d5dd571cc8d4641cd55f49d3b
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2024-03-17 17:37:42 -0400

    add DAI Faucet

---
+++ src/DAIFaucet.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+interface IERC20 {
+    function transfer(address to, uint256 value) external returns (bool);
+}
+
+contract DAIFaucet {
+    uint256 public tokenAmount = 100000 * 10 ** 18;
+    IERC20 public tokenInstance;
+    event TokensWithdrawn(address indexed recipient, uint256 amount);
+
+    constructor(address _tokenInstance) {
+        tokenInstance = IERC20(_tokenInstance);
+    }
+
+    function requestTokens() public {
+        tokenInstance.transfer(msg.sender, tokenAmount);
+        emit TokensWithdrawn(msg.sender, tokenAmount);
+    }
+}
