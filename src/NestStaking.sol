75dafa2a7e37b12b7774fd8482443d9570f2d233
Purva-Chaudhari <48947123+Purva-Chaudhari@users.noreply.github.com>
2024-07-09 17:23:48 +0530

    storage (#27)

---
+++ src/NestStaking.sol
@@ -0,0 +1,163 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "./NestStakingStorage.sol";
+import "./CheckInStorage.sol";
+
+contract NestStaking is UUPSUpgradeable, AccessControlUpgradeable {
+    using NestStakingStorage for NestStakingStorage.Storage;
+
+    uint256 private constant SECONDS_PER_DAY = 86400;
+
+    event Staked(address indexed user, uint256 amount);
+    event Unstaked(address indexed user, uint256 amount);
+    event Claimed(address indexed user, uint256 goonRewards, uint256 nestRewards, uint256 miles);
+
+    function initialize(
+        address stRwaTokenAddress,
+        address goonTokenAddress,
+        address nestTokenAddress,
+        address dateTimeAddress,
+        address checkInAddress
+    ) external initializer {
+        __UUPSUpgradeable_init();
+        __AccessControl_init();
+
+        NestStakingStorage.Storage storage rs = NestStakingStorage.getStorage();
+        rs.stRwaToken = stRWA(stRwaTokenAddress);
+        rs.goonToken = GOON(goonTokenAddress);
+        rs.nestToken = NEST(nestTokenAddress);
+        rs.dateTime = IDateTime(dateTimeAddress);
+        rs.checkIn = ICheckIn(checkInAddress);
+        rs.APR = 40;
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
+
+    function stake(uint256 amount) external {
+        require(amount > 0, "Amount must be greater than 0");
+
+        NestStakingStorage.Storage storage rs = NestStakingStorage.getStorage();
+        require(rs.goonToken.transferFrom(msg.sender, address(this), amount), "goonUSD transferFrom failed");
+
+        rs.stRwaToken.mint(msg.sender, amount);
+        accumulateRewards(msg.sender);
+        rs.checkIn.incrementTaskPoints(msg.sender, CheckInStorage.Task.NEST);
+
+        emit Staked(msg.sender, amount);
+    }
+
+    function unstake(uint256 amount) external {
+        NestStakingStorage.Storage storage rs = NestStakingStorage.getStorage();
+        require(rs.stRwaToken.balanceOf(msg.sender) >= amount, "Insufficient stRWA balance");
+
+        accumulateRewards(msg.sender);
+        rs.stRwaToken.burn(msg.sender, amount);
+        require(rs.goonToken.transfer(msg.sender, amount), "goonUSD transfer failed");
+
+        emit Unstaked(msg.sender, amount);
+    }
+
+    function claim() public {
+        NestStakingStorage.Storage storage rs = NestStakingStorage.getStorage();
+        accumulateRewards(msg.sender);
+
+        (uint256 goonRewards, uint256 nestRewards, uint256 miles) = getUnclaimedRewards(msg.sender);
+
+        if (goonRewards > 0) {
+            require(rs.goonToken.transfer(msg.sender, goonRewards), "$GOON transfer failed");
+            require(rs.nestToken.transfer(msg.sender, nestRewards), "$NEST transfer failed");
+            rs.checkIn.incrementNestPoints(msg.sender, miles);
+            rs.unclaimedRewards[msg.sender] = 0;
+
+            emit Claimed(msg.sender, goonRewards, nestRewards, miles);
+        }
+    }
+
+    function getUnclaimedRewards(address user) public view returns (uint256, uint256, uint256) {
+        NestStakingStorage.Storage storage rs = NestStakingStorage.getStorage();
+        uint256 rewards = rs.unclaimedRewards[user];
+        uint256 goonRewards = (rewards * 2) / 1000;
+        uint256 nestRewards = rewards * 2;
+        uint256 miles = 1 + ((rewards * 2) / 100 - 1) / 1e18;
+
+        return (goonRewards, nestRewards, miles);
+    }
+
+    function setAPR(uint256 APR) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        NestStakingStorage.Storage storage rs = NestStakingStorage.getStorage();
+        rs.APR = APR;
+    }
+
+    function rebase() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        NestStakingStorage.Storage storage rs = NestStakingStorage.getStorage();
+        require(rs.dateTime.getHour(block.timestamp) == 0, "NestStaking: rebase can only be called once per day");
+
+        uint16 prevYear = rs.dateTime.getYear(rs.lastRebaseTimestamp);
+        uint8 prevMonth = rs.dateTime.getMonth(rs.lastRebaseTimestamp);
+        uint8 prevDay = rs.dateTime.getDay(rs.lastRebaseTimestamp);
+        uint16 currentYear = rs.dateTime.getYear(block.timestamp);
+        uint8 currentMonth = rs.dateTime.getMonth(block.timestamp);
+        uint8 currentDay = rs.dateTime.getDay(block.timestamp);
+
+        require(
+            isNextDay(prevYear, prevMonth, prevDay, currentYear, currentMonth, currentDay),
+            "NestStaking: rebase already called today"
+        );
+
+        uint256 rewardMultiplierIncrement = rs.APR / 365;
+        rs.stRwaToken.addRewardMultiplier(rewardMultiplierIncrement);
+        rs.lastRebaseTimestamp = block.timestamp;
+    }
+
+    function getAccumulatedRewards(address user) internal view returns (uint256) {
+        NestStakingStorage.Storage storage rs = NestStakingStorage.getStorage();
+        uint256 userStake = rs.stRwaToken.balanceOf(user);
+        uint256 previousMidnight = block.timestamp - (block.timestamp % SECONDS_PER_DAY);
+        uint256 lastMidnight = rs.lastAccumulated[user] - (rs.lastAccumulated[user] % SECONDS_PER_DAY);
+        uint256 n = (previousMidnight - lastMidnight) / SECONDS_PER_DAY;
+
+        uint256 rateTo18 = 36500e18 / (36500 + rs.APR);
+        uint256 rewards = userStake * (1e18 - power(rateTo18, n)) / (1e18 - rateTo18);
+
+        return rewards;
+    }
+
+    function accumulateRewards(address user) internal {
+        NestStakingStorage.Storage storage rs = NestStakingStorage.getStorage();
+        uint256 rewards = getAccumulatedRewards(user);
+        rs.unclaimedRewards[user] += rewards;
+        rs.lastAccumulated[user] = block.timestamp;
+    }
+
+    function power(uint256 rateTo18, uint256 n) internal pure returns (uint256) {
+        uint256 resTo18 = 1e18;
+        for (uint256 i = 0; i < n; i++) {
+            resTo18 = (resTo18 * rateTo18) / 1e18;
+        }
+        return resTo18;
+    }
+
+    function isNextDay(
+        uint16 lastYear,
+        uint8 lastMonth,
+        uint8 lastDay,
+        uint16 currentYear,
+        uint8 currentMonth,
+        uint8 currentDay
+    ) internal view returns (bool) {
+        NestStakingStorage.Storage storage rs = NestStakingStorage.getStorage();
+        uint256 lastDateTimestamp = rs.dateTime.toTimestamp(lastYear, lastMonth, lastDay);
+        uint256 nextDayTimestamp = lastDateTimestamp + SECONDS_PER_DAY;
+
+        uint16 nextDayYear = rs.dateTime.getYear(nextDayTimestamp);
+        uint8 nextDayMonth = rs.dateTime.getMonth(nextDayTimestamp);
+        uint8 nextDayDay = rs.dateTime.getDay(nextDayTimestamp);
+
+        return (nextDayYear == currentYear) && (nextDayMonth == currentMonth) && (nextDayDay == currentDay);
+    }
+}
---
+++ src/NestStakingStorage.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {ICheckIn} from "./interfaces/ICheckIn.sol";
+import {IDateTime} from "./interfaces/IDateTime.sol";
+import {stRWA} from "./stRWA.sol";
+import {GOON} from "./GOON.sol";
+import {NEST} from "./NEST.sol";
+
+library NestStakingStorage {
+    struct Storage {
+        stRWA stRwaToken;
+        GOON goonToken;
+        NEST nestToken;
+        address admin;
+        uint256 APR;
+        uint256 lastRebaseTimestamp;
+        mapping(address => uint256) lastAccumulated;
+        mapping(address => uint256) unclaimedRewards;
+        IDateTime dateTime;
+        ICheckIn checkIn;
+    }
+
+    function getStorage() internal pure returns (Storage storage rs) {
+        bytes32 position = keccak256("neststaking.storage");
+        assembly {
+            rs.slot := position
+        }
+    }
+}
